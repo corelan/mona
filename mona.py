a94259fa5e03c6bc35507d361c1263e68343e373
corelanc0d3r <peter.ve@corelan.be>
2022-10-09 17:51:14 +0200

    version bump

--- mona.py
+++ mona.py
@@ -28,12 +28,12 @@ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY 
 WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
-$Revision: 616 $
-$Id: mona.py 615 2021-09-04 08:29:00Z corelanc0d3r $ 
+$Revision: 617 $
+$Id: mona.py 617 2022-10-09 17:49:00Z corelanc0d3r $ 
 """
 
 __VERSION__ = '2.0'
-__REV__ = filter(str.isdigit, '$Revision: 616 $')
+__REV__ = filter(str.isdigit, '$Revision: 617 $')
 __IMM__ = '1.8'
 __DEBUGGERAPP__ = ''
 arch = 32
